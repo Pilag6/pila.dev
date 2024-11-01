6bb309bdc43c6d03e2649938e7ebca3201c1b6f0
Pilag6 <pilag6@gmail.com>
2024-11-01 09:45:11 +0100

    chore: Add Reviews component and update curriculum PDF file

--- src/components/Hero/HeroBtns.jsx
+++ src/components/Hero/HeroBtns.jsx
@@ -1,7 +1,7 @@
 import "./HeroBtns.css"
 import drawHeroArrow from "../../assets/draw-hero-arrow.svg";
 import drawHeroCircle from "../../assets/draw-hero-circle.svg";
-import Curriculum from "../../assets/ezequielGonzalez_CV.pdf";
+import Curriculum from "../../assets/ezequielGonzalez.pdf";
 
 const HeroBtns = () => {
     return (
